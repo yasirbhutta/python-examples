7ea228622b8f944ece5479ab0a56a3a4bcd2ad75
Yasir Bhutta <yasirbhutta@gmail.com>
2022-09-19 22:21:54 +0500

    indentation

---
+++ basics/indent.py
@@ -0,0 +1,15 @@
+# Pyton indentation
+# Python indentation is a way of telling a Python interpreter that 
+# the group of statements belongs to a particular block of code.
+#
+# Python uses indentation to indicate a block of code.
+
+pwd ="apple"
+if pwd == 'apple':
+    print('Logging on ...')
+else:
+    print('Incorrect password.')
+
+print('All done!')
+
+# for more details: https://www.freecodecamp.org/news/indentation-in-python/
