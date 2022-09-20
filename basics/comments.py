5abe04361aa05ad51af2c6aa884143dd5886d6c0
Yasir Bhutta <yasirbhutta@gmail.com>
2022-09-20 20:29:07 +0500

    comments

---
+++ basics/comments.py
@@ -0,0 +1,10 @@
+# Python Comments
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+
+# This is a comment
+print("Hello, World!")
+
+x = 8  # Initialize x with an arbitrary number
