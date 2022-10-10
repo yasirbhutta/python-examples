9ee8ef0175097f745f9d095a6cdb991a83ccf0d1
Yasir Bhutta <yasirbhutta@gmail.com>
2022-10-10 21:05:47 +0500

    dict1

---
+++ data-types/dict1.py
@@ -0,0 +1,13 @@
+# https://web.facebook.com/groups/pythontips
+thisdict = {
+  "rollno": 5,
+  "name": "Muhammad Ahmad",
+  "university": "Ghazi University DG Khan",
+  "age":25  
+}
+print(thisdict["name"]) ##Output: Muhammad Ahmad
+
+# loop
+names = {'hamza': 'lion', 'yasir': 'well to do'}
+for n, m in names.items():
+  print(f'name: {n}, meaning: {m}')
