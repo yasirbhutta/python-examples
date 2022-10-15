77a16f9cf682096efda4374094887a36f2532523
Yasir Bhutta <yasirbhutta@gmail.com>
2022-10-15 21:30:46 +0500

    range1

--- data-types/range1.py
+++ data-types/range1.py
@@ -4,15 +4,11 @@
 ## class range(start, stop[, step])
 
 print(list(range(10)))
-
 print(list(range(1,10)))
 
 print(list(range(0,25,5)))
-
 print(list(range(0,10,3)))
-
 print(list(range(0,-10,-1)))
 
 print(list(range(0)))
-
 print(list(range(1,0)))
