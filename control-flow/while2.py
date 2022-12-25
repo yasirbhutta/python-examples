4615d292e239d9026abab087b09da623b94865ab
Yasir Bhutta <yasirbhutta@gmail.com>
2022-12-25 19:10:34 +0500

    update

---
+++ control-flow/while2.py
@@ -0,0 +1,10 @@
+# Example # - python while loop
+print('-- Help: type "quit" to exit --')
+while True:
+    num1 = input('Enter number to check is odd or even ')
+    if num1.lower() == 'quit':
+        break
+    elif int(num1)%2 == 0:
+        print ('Number is even: ', num1)
+    else:
+        print('Number is odd: ',num1)
