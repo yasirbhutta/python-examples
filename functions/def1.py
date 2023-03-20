dd6de6508bcf6aa9596b203def484aee28ec4768
adnananjum242 <127759301+adnananjum242@users.noreply.github.com>
2023-03-20 10:49:54 +0500

    function

---
+++ functions/def1.py
@@ -0,0 +1,33 @@
+def add_numbers(x, y):
+    return x + y
+result = add_numbers(5, 10)
+print(result)
+print(add_numbers(5, 10))
+
+
+
+
+# You can also follow me on:
+
+# Website: https://yasirbhutta.github.io/
+# Facebook: https://www.facebook.com/yasirbhutta786/
+# YouTube:https://youtube.com/@YasirBhutta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
