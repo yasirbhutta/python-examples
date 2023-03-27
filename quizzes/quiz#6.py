cbc223b26aaaefb199c94bcccfe984c2450dac7b
adnananjum242 <127759301+adnananjum242@users.noreply.github.com>
2023-03-27 09:46:32 +0500

    quiz

---
+++ quizzes/quiz#6.py
@@ -0,0 +1,17 @@
+for i in range(5):
+  if i == 3:
+    break
+  print(i)
+
+
+  # This program prints the values of i from 0 to 2.
+# The for loop iterates over a range of numbers from 0 to 4, inclusive of both endpoints.
+# The if statement checks if the current value of i is equal to 3.
+# If i is equal to 3, the break statement is executed, which terminates the loop.
+# If i is less than 3, the print() function is used to output the value of i to the console.
+
+# You can also follow me on:
+
+# Website: https://yasirbhutta.github.io/
+# Facebook: https://www.facebook.com/yasirbhutta786/
+# YouTube:https://youtube.com/@YasirBhutta
