75e2f948fe8297b96d9129d43445aec44e1aef7b
adnananjum242 <127759301+adnananjum242@users.noreply.github.com>
2023-03-27 09:32:54 +0500

    quiz

---
+++ functions/quiz#3.py
@@ -0,0 +1,16 @@
+for i in range(10):
+  if i % 2 == 0:
+    print(i)
+
+
+    # This program prints all even numbers from 0 to 9, inclusive.
+# It uses a for loop to iterate over a range of numbers from 0 to 9,
+# and an if statement to check if each number is even.
+# If a number is even, it is printed to the console using the print() function.
+
+
+# You can also follow me on:
+
+# Website: https://yasirbhutta.github.io/
+# Facebook: https://www.facebook.com/yasirbhutta786/
+# YouTube:https://youtube.com/@YasirBhutta
