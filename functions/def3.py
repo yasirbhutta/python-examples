ac317b085f39232ae9018f55157a917d2ee5213b
adnananjum242 <127759301+adnananjum242@users.noreply.github.com>
2023-03-28 17:56:11 +0500

    update

---
+++ functions/def3.py
@@ -0,0 +1,23 @@
+def find_length(word):
+    length = 0
+    for char in word:
+        length += 1
+    return length 
+find_length("python language")
+
+
+
+# This program defines a function called 'find_length' that takes a string argument 'word'.
+# The function initializes a variable 'length' to 0 to count the number of characters in the string.
+# The function then iterates over each character in the string using a for loop.
+# For each character, the function increments the value of 'length' by 1.
+# After all characters have been counted, the function returns the value of 'length'.
+# The function call 'find_length("python language")' passes the string "python language" as an argument.
+# The function returns the value 15, which is the length of the string "python language".
+
+
+# You can also follow me on:
+
+# Website: https://yasirbhutta.github.io/
+# Facebook: https://www.facebook.com/yasirbhutta786/
+# YouTube:https://youtube.com/@YasirBhutta
