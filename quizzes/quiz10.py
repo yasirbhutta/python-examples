87e0c47b347d419bbd40eda0963964a9b11c35f8
adnananjum242 <127759301+adnananjum242@users.noreply.github.com>
2023-03-28 10:05:43 +0500

    quiz

---
+++ quizzes/quiz10.py
@@ -0,0 +1,20 @@
+x = 5
+y = 10
+result = x > 3 or y < 5
+result
+
+
+# This program defines two variables 'x' and 'y' with values 5 and 10, respectively.
+# The program then creates a new variable 'result' that is assigned the result of a boolean expression.
+# The expression checks whether x is greater than 3 OR y is less than 5, using the logical OR operator.
+# Since y is not less than 5, the expression returns True as long as x is greater than 3.
+# The value of the variable 'result' is True, since x is greater than 3.
+# The variable 'result' is not printed or used further in the program.
+
+
+
+# You can also follow me on:
+
+# Website: https://yasirbhutta.github.io/
+# Facebook: https://www.facebook.com/yasirbhutta786/
+# YouTube:https://youtube.com/@YasirBhutta
