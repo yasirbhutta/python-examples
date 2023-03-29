0d3a85b5bef298557a5445ed3c368b653d826ecb
adnananjum242 <127759301+adnananjum242@users.noreply.github.com>
2023-03-29 11:19:10 +0500

    Excersie

---
+++ exerciises/excercise2.py
@@ -0,0 +1,20 @@
+def fun(arr):
+    arr = arr[::-1]
+    print(arr)
+arr = [1, 2, 3, 4, 5]
+fun(arr)
+
+
+# This program defines a function called 'fun' that takes a list as its argument.
+# The function reverses the order of the elements in the list using the slice notation 'arr[::-1]'.
+# The reversed list is then printed to the console using the 'print' function.
+# The program creates a list called 'arr' containing the numbers 1, 2, 3, 4, and 5.
+# The function 'fun' is called with 'arr' as its argument, so the function will reverse the order of the elements in 'arr' and print the result to the console.
+# The output of the program will be the reversed list [5, 4, 3, 2, 1] printed to the console.
+
+
+  # You can also follow me on:
+
+# Website: https://yasirbhutta.github.io/
+# Facebook: https://www.facebook.com/yasirbhutta786/
+# YouTube:https://youtube.com/@YasirBhutta
