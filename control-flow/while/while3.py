cd8ab2c7d9b34a89e2a143604f09554d9d0e5c94
Muhammad Yasir Bhutta <yasirbhutta@gmail.com>
2024-01-31 09:44:58 +0500

    update

--- control-flow/while/while3.py
+++ control-flow/while/while3.py
@@ -1,4 +1,11 @@
 # Youtube@YasirBhutta
 # Sum of given numbers till the number entered is zero
+sum = 0
+number = int(input("enter the number: "))
 
+while number != 0:
+    sum += number
+    number = int(input("Enter another number:"))
+
+print(f'The sum of the number is {sum}')
 
